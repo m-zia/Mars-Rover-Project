87fd9850102fdf13c5671bbf41d8c35115d83c3b
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-30 18:36:36 +0100

    added more tests for rotating the rover

--- tests/rover/moveRover.test.ts
+++ tests/rover/moveRover.test.ts
@@ -3,38 +3,28 @@
 import { moveRover } from "../../src/rover/moveRover"
 import { Plateau, Rover } from "../../src/types/rover";
 
+
+
+//need MORE multiple step tests!!!!
+
+
 describe('Move Rover based on given position and instructions', () => {
   const plateau: Plateau = { maxX: 5, maxY: 5 };
 
-  //tests for moving the rover have been exhaustively extended below
+ 
+  it('Move the rover multiple steps', () => {
+    const rover: Rover = { x: 1, y: 2, direction: 'N' };
+    const instructions = 'LMLMLMLMM';
+    const finalRover = moveRover(plateau, rover, instructions);
+    expect(finalRover).toEqual({ x: 1, y: 3, direction: 'N' });
+  });
 
-  // it('Move the rover North', () => {
-  //   const rover: Rover = { x: 1, y: 2, direction: 'N' };
-  //   const instructions = 'M';
-  //   const finalRover = moveRover(plateau, rover, instructions);
-  //   expect(finalRover).toEqual({ x: 1, y: 3, direction: 'N' });
-  // });
 
-  // it('Move the rover South', () => {
-  //   const rover: Rover = { x: 1, y: 2, direction: 'S' };
-  //   const instructions = 'M';
-  //   const finalRover = moveRover(plateau, rover, instructions);
-  //   expect(finalRover).toEqual({ x: 1, y: 1, direction: 'S' });
-  // });
+});
 
-  // it('Move the rover East', () => {
-  //   const rover: Rover = { x: 1, y: 2, direction: 'E' };
-  //   const instructions = 'M';
-  //   const finalRover = moveRover(plateau, rover, instructions);
-  //   expect(finalRover).toEqual({ x: 2, y: 2, direction: 'E' });
-  // });
 
-  // it('Move the rover West', () => {
-  //   const rover: Rover = { x: 1, y: 2, direction: 'W' };
-  //   const instructions = 'M';
-  //   const finalRover = moveRover(plateau, rover, instructions);
-  //   expect(finalRover).toEqual({ x: 0, y: 2, direction: 'W' });
-  // });
+describe('Rotate the rover to the left based on given position and instructions', () => {
+  const plateau: Plateau = { maxX: 5, maxY: 5 };
 
   it('Rotate the rover to the left', () => {
     const rover: Rover = { x: 1, y: 2, direction: 'N' };
@@ -43,37 +33,65 @@ describe('Move Rover based on given position and instructions', () => {
     expect(finalRover).toEqual({ x: 1, y: 2, direction: 'W' });
   });
 
-  it('Rotate the rover to the right', () => {
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 3, y: 2, direction: 'E' };
+    const instructions = 'L';
+    const finalRover = moveRover(plateau, rover, instructions);
+    expect(finalRover).toEqual({ x: 3, y: 2, direction: 'N' });
+  });
+
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 5, y: 5, direction: 'W' };
+    const instructions = 'L';
+    const finalRover = moveRover(plateau, rover, instructions);
+    expect(finalRover).toEqual({ x: 5, y: 5, direction: 'S' });
+  });
+
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 0, y: 1, direction: 'S' };
+    const instructions = 'L';
+    const finalRover = moveRover(plateau, rover, instructions);
+    expect(finalRover).toEqual({ x: 0, y: 1, direction: 'E' });
+  });
+
+});
+
+
+describe('Rotate the rover to the right based on given position and instructions', () => {
+  const plateau: Plateau = { maxX: 5, maxY: 5 };
+
+  it('Rotate the rover to the left', () => {
     const rover: Rover = { x: 1, y: 2, direction: 'N' };
     const instructions = 'R';
     const finalRover = moveRover(plateau, rover, instructions);
     expect(finalRover).toEqual({ x: 1, y: 2, direction: 'E' });
   });
 
-  it('Move the rover multiple steps', () => {
-    const rover: Rover = { x: 1, y: 2, direction: 'N' };
-    const instructions = 'LMLMLMLMM';
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 3, y: 2, direction: 'E' };
+    const instructions = 'R';
     const finalRover = moveRover(plateau, rover, instructions);
-    expect(finalRover).toEqual({ x: 1, y: 3, direction: 'N' });
+    expect(finalRover).toEqual({ x: 3, y: 2, direction: 'S' });
   });
 
-  it('Should not move the rover beyond the plateau boundaries', () => {
-    const rover: Rover = { x: 5, y: 5, direction: 'N' };
-    const instructions = 'M';
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 5, y: 5, direction: 'W' };
+    const instructions = 'R';
     const finalRover = moveRover(plateau, rover, instructions);
     expect(finalRover).toEqual({ x: 5, y: 5, direction: 'N' });
   });
 
+  it('Rotate the rover to the left', () => {
+    const rover: Rover = { x: 0, y: 1, direction: 'S' };
+    const instructions = 'R';
+    const finalRover = moveRover(plateau, rover, instructions);
+    expect(finalRover).toEqual({ x: 0, y: 1, direction: 'W' });
+  });
 
 });
 
 
-//need more tests for the above function; essentially need ALL the equivalent movement & border tests that we did in
-// the roverFunctions.test.ts file
-
-
-
-describe('moving the rover when the plateau is 5 ', () => {
+describe('Moving the rover when the plateau is 5 ', () => {
   const plateau: Plateau = { maxX: 5, maxY: 5 };
 
   //x=0
